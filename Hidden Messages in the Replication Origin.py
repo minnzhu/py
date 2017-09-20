98cc6e0d242c5b735d4ff08e230e6aa1d2a77c89
minnzhu <32130240+minnzhu@users.noreply.github.com>
2017-09-20 14:34:56 +0200

    Create Hidden Messages in the Replication Origin.py

---
+++ Hidden Messages in the Replication Origin.py
@@ -0,0 +1,29 @@
+```python
+
+# Input:  A string Text and an integer k
+# Output: CountDict(Text, k)
+def CountDict(Text, k):
+    Count = {}
+    for i in range(len(Text)-k+1):
+        Pattern = Text[i:i+k]
+        Count[i] = PatternCount(Pattern, Text)
+    return Count
+
+# Input:  A string Text and an integer k
+# Output: A list containing all most frequent k-mers in Text
+def FrequentWords(Text, k):
+    FrequentPatterns = []
+    Count = CountDict(Text, k)
+    m = max(Count.values())
+    for i in Count:
+        if Count[i] == m:
+            FrequentPatterns.append(Text[i:i+k])
+    return FrequentPatterns
+
+
+def PatternCount(Pattern, Text):
+    count = 0
+    for i in range(len(Text)-len(Pattern)+1):
+        if Text[i:i+len(Pattern)] == Pattern:
+            count = count+1
+    return count 
